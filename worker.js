0702a5235ba4952fb2e5ec15b2ec99d04705a1db
ushiostarfish <ushiostarfish@gmail.com>
2024-04-26 23:44:56 +0900

    non blocking psnr calculations

---
+++ worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('message',(e) => {
+    const pixels = e.data.pixels;
+
+    var mseR = 0;
+    var mseG = 0;
+    var mseB = 0;
+    const numPixels = pixels.length / 4;
+    for( let i = 0; i < pixels.length; i += 4 ) {
+        rd = pixels[i];
+        gd = pixels[i+1];
+        bd = pixels[i+2];
+        mseR += rd * rd;
+        mseG += gd * gd;
+        mseB += bd * bd;
+    }
+    const mse = ( mseR + mseG + mseB ) / 3 / numPixels;
+    const maxI = 255
+    const PSNR = 10 * Math.log10( maxI * maxI / mse );
+    e.data.PSNR = PSNR;
+    
+    self.postMessage(e.data);
+}, false);
